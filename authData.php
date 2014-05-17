7d2b228f8127019092b45a8e303d36a319bf12cf
Varun Gupta <varunalive@outlook.com>
2014-05-17 11:55:15 +0530

    TalentOn Internship repo

---
+++ authData.php
@@ -0,0 +1,15 @@
+<?php
+//GitHub
+$client_id_gh="502d039facf801066513";
+$client_secret_gh="5a598ffbf9e51d451da0a1c7f5b5d683d818e272";
+$url_gh="https://github.com/login/oauth/access_token";
+//Angel List
+$client_id_al="d67e9d194d5840e4cce89a9ff5435c07";
+$client_secret_al="62c31296745012bdc461df015a1ee08e";
+$url_al="https://angel.co/api/oauth/token";
+//Linked In
+$state="DCEEFWF45453sdffef424";//(Create Your Own Large String and also change it in img src url)
+$url_li="https://www.linkedin.com/uas/oauth2/accessToken";
+$client_id_li="75quid8jkyfzi4";
+$client_secret_li="WkSZ44xuIz0wl5H7";
+?>
